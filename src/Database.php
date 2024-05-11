8b2997fed7d0b4adcfcf30de908ef134bd1aa929
Maja Roksz <5009217@teb-gdansk.ad>
2024-05-11 10:43:48 +0200

    databasenew

--- src/Database.php
+++ src/Database.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\Exception\ConfigurationException;
+
 use App\Exception\StorageException;
-use Exception;
+use App\Exception\ConfigurationException;
 use PDO;
 use PDOException;
 use Throwable;
@@ -20,7 +20,7 @@ class Database
             $this->validateConfig($config);
             $this->createConnection($config);
         } catch (PDOException $e) {
-            throw new StorageException("Problem z połączeniem do bazy!");
+            throw new StorageException('Connection error');
         }
     }
 
@@ -30,7 +30,7 @@ class Database
             $title = $this->conn->quote($data['title']);
             $description = $this->conn->quote($data['description']);
             $created = date('Y-m-d H:i:s');
-            $query = "INSERT INTO notes (title, description, created) VALUES($title, $description, '$created')"; //APOSTROF
+            $query = "INSERT INTO notes(title,description,created) VALUES($title, $description, '$created')";
             $result = $this->conn->exec($query);
         } catch (Throwable $e) {
             throw new StorageException('Nie udało się utworzyć nowej notatki', 400, $e);
@@ -38,21 +38,21 @@ class Database
     }
     private function createConnection(array $config): void
     {
-        $dsn = "mysql:dbname={$config['database']}host={$config['host']}";
+        $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
         $this->conn = new PDO(
             $dsn,
-            $congif['user'],
+            $config['user'],
             $config['password'],
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]
-            );
-            // $connection = new PDO('lalalalala');
+        );
     }
+
     private function validateConfig(array $config): void
     {
-        if (empty($config['database']) || empty($config['user']) || empty($config['host'])) {
-            throw new ConfigurationException("Problem z konfiguracją bazy - skontaktuj się z administratorem.");
+        if (empty($config['database'])  || empty($config['user']) || empty($config['host'])) {
+            throw new ConfigurationException('Problem z konfigurajcą - skontaktuj się z administratorem');
         }
     }
 }
