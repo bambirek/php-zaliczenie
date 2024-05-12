8ef42787d8b065683aefb01156776f01534a3570
Maja Roksz <5009217@teb-gdansk.ad>
2024-05-12 09:52:09 +0200

    new pages added

--- src/Database.php
+++ src/Database.php
@@ -6,6 +6,7 @@ namespace App;
 
 
 use App\Exception\StorageException;
+use App\Exception\NotFoundException;
 use App\Exception\ConfigurationException;
 use PDO;
 use PDOException;
@@ -48,6 +49,22 @@ class Database
         } catch (Throwable $e) {
             throw new StorageException('Nie udało się pobrać danych o notatkach.', 400, $e);
         }
+    }
+
+    public function getNote(int $id): array
+    {
+        try {
+            $query = "SELECT * FROM notes WHERE id=$id";
+            $result = $this->conn->query($query);
+            $note = $result->fetch(PDO::FETCH_ASSOC);
+        } catch (Throwable $e) {
+            throw new StorageException('Nie udało się pobrać notatki', 400, $e);
+        }
+        if (!$note) {
+            throw new NotFoundException('Notatka o id: $id nie istnieje.');
+        }
+
+        return $note;
     }
     private function createConnection(array $config): void
     {
